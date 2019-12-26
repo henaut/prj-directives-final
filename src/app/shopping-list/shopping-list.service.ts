72772346573d72ea5b3887186aee1bada70f6b59
Kevin <khenaut@emasphere.com>
2019-12-26 09:09:21 +0100

    Shopping list edit

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,9 @@
 import {Ingredient} from '../shared/ingredient.model';
-import {EventEmitter} from '@angular/core';
 import {Subject} from 'rxjs';
 
 export class ShoppingListService {
-  ingredientsChanged = new Subject<Ingredient[]>()
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -12,6 +12,10 @@ export class ShoppingListService {
   getIngredients() {
     return this.ingredients.slice();
   }
+  getIngredient(index) {
+    return this.ingredients[index];
+  }
+
   addIngredient( ingredient: Ingredient): void {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
@@ -19,6 +23,16 @@ export class ShoppingListService {
 
   addIngredients( ingredient: Ingredient[]): void {
     this.ingredients.push(...ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient (index: number, newIngredient: Ingredient): void {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
